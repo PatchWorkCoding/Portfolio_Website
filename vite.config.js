44ef6f0d15c11bc29810c6fb90c4db421a253c96
developer <dev@NEW-MACHINE-01.com>
2025-09-24 10:26:17 -0500

    Update Build Settings

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,19 @@
-export default {
-    base: '/Portfolio_Website/'
-    // config options
-}
+
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { defineConfig } from 'vite'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
+
+export default defineConfig({
+    base: '/Portfolio_Website/',
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+                nested: resolve(__dirname, 'Dreams_Of_Disquiet/Dreams_Of_Disquiet.html'),
+                nested2: resolve(__dirname, 'FMA/FMA.html'),
+            },
+        },
+    },
+})
